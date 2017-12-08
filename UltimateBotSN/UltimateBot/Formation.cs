cf2eb021b1afc3d6af20935110c674cd412fbbee
Shaked <shaked1mi@gmail.com>
2017-12-08 23:59:23 +0200

    Added methord recording in Formation class

--- UltimateBotSN/UltimateBot/Formation.cs
+++ UltimateBotSN/UltimateBot/Formation.cs
@@ -12,6 +12,14 @@ namespace MyBot
         Pirate bodyguard;
         Pirate bodyguard2;
         Pirate tail;
+
+        /// <summary>
+        /// Constructor to create the Formation.
+        /// </summary>
+        /// <param name="p1">Carrier Pirate.</param>
+        /// <param name="p2">Bodyguard Pirate one.</param>
+        /// <param name="p3">Bodyguard Pirate two.</param>
+        /// <param name="p4">Tail Pirate.</param>
         public Formation(Pirate p1, Pirate p2, Pirate p3, Pirate p4)
         {
             this.carrier = p1;
@@ -19,10 +27,12 @@ namespace MyBot
             this.bodyguard2 = p3;
             this.tail = p4;
         }
+
         /// <summary>
-        /// form up in 4 pirates formation and sail the the given target.
+        /// Form up in 4 pirates formation and sail the the given target.
         /// </summary>
-        /// <param name="target"> a given location the formation is wanted to head for</param>
+        /// <remarks> This code was created by @Idan, for any question about wtf this code is for just ask him :)</remarks>
+        /// <param name="target"> A given location the formation is wanted to head for.</param>
         public void FormUpAndSail(Location target)
         {
             Location upperDot = target.Towards(carrier, carrier.PushRange / 3);
@@ -35,10 +45,10 @@ namespace MyBot
             bodyguard2.Sail(upperDot.Subtract(V));
             tail.Sail(carrier.GetLocation().Subtract(U));
 
-            if (tail.Distance(carrier.GetLocation().Subtract(U)) <= 50 &&
+            if (tail.Distance(carrier.GetLocation().Subtract(U)) <= 50 && 
                 bodyguard.Distance(upperDot.Add(V)) <= 50 &&
                 bodyguard2.Distance(upperDot.Subtract(V)) < 50)
-                carrier.Sail(target);
+                    carrier.Sail(target);
         }
     }
 }
