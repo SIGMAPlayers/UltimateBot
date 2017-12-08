09c2339b5c3e5adf096b2bd9fef377b89d6778e5
idan elias <idanelias4@gmail.com>
2017-12-08 22:35:35 +0200

    push2

--- UltimateBotSN/UltimateBot/MyBot.cs
+++ UltimateBotSN/UltimateBot/MyBot.cs
@@ -13,7 +13,7 @@ namespace MyBot
         //ss
         //not the nazi one. sss
         //push1
-
+        //push2
         //comes on work with me LOL
         /// <summary>
         /// Makes the bot run a single turn.
