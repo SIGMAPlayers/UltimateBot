f2812691213f382057d06901dfc29c40f97278bc
Shaked <shaked1mi@gmail.com>
2017-12-19 23:51:21 +0200

    Added all classes as .cs files

---
+++ UltimateBotSN/UltimateBot/DefenderList.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Pirates;
+
+namespace MyBot
+{
+    //A list of all defenders
+    public class DefenderList : List<Defender>
+    {
+        public DefenderList(PirateGame game)
+        {
+            PirateList def = new PirateList(game.GetAllMyPirates().OrderBy(Pirate => Pirate.Location.Distance(game.GetMyCapsule().Location)));
+            def.RemoveRange(0, 4);
+            for (int i = 0; i < (def.Count / 2); i++)
+            {
+                this.Add(new Defender(def[i], Roles.front));
+            }
+            for (int i = 0; i < def.Count - (def.Count / 2); i++)
+            {
+                this.Add(new Defender(def[i], Roles.backup));
+            }
+
+
+        }
+        public DefenderList() { }
+
+        public DefenderList(IEnumerable<Defender> list) : base(list) { }
+    }
+}
---
+++ UltimateBotSN/UltimateBot/PirateList.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Pirates;
+
+namespace MyBot
+{
+    //A list of pirates 
+    public class PirateList : List<Pirate>
+    {
+        public PirateList() { }
+
+        public PirateList(IEnumerable<Pirate> list) : base(list) { }
+    }
+}
