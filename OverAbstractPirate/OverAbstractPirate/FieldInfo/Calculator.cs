a97ead0834a0407a2f13d84c41787e2121043fff
Matan Oded <matanoded@gmail.com>
2018-02-04 19:06:39 +0200

    Added 2 methods to Calculator class - "PredictLocationByMovement" and "PredictLocationAfterPush"

--- OverAbstractPirate/OverAbstractPirate/FieldInfo/Calculator.cs
+++ OverAbstractPirate/OverAbstractPirate/FieldInfo/Calculator.cs
@@ -43,5 +43,74 @@ namespace MyBot
             Location upperDot = carrier.Pirate.Location.Towards(carrier.Destination, carrier.Pirate.PushRange*2);
             return upperDot;
         }
+
+        /// <summary>
+        /// Takes an object, a list of games, predicts the location of the object in the next turn
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public Location PredictLocationByMovement (SpaceObject obj)
+        {
+            List<SpaceObject> spaceObjects = null;
+            Location currentLocation = obj.Location;
+            Location previousLocation = null;
+
+            // First, check the type of obj
+            if (obj is Pirate)
+            {
+                spaceObjects.AddRange(GameSettings.GameList[GameSettings.GameList.Count-2].GetEnemyLivingPirates().ToList());
+            }
+            else if (obj is Asteroid)
+            {
+                spaceObjects.AddRange(GameSettings.GameList[GameSettings.GameList.Count - 2].GetLivingAsteroids().ToList());
+            }
+            else return null;
+
+            foreach (SpaceObject movingObj in spaceObjects) // Check on every SpaceObject on the list defined before:
+            {
+                if (movingObj.Id == obj.Id) // Is it the original object?
+                {
+                    previousLocation = movingObj.Location; // Then get it's past Location! put it in "previousLocation"
+                    break;
+                }
+            }
+
+            // Now, calculate it's predicted Location
+            return currentLocation.Add(currentLocation.Subtract(previousLocation));
+
+        }
+
+
+        /// <summary>
+        /// Takes a SpaceObject, and calculate it's final location based on the push and the current movement
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="pushTo"></param>
+        /// <param name="moveTo"></param>
+        /// <returns></returns>
+        public Location PredictLocationAfterPush (SpaceObject obj, Location pushTo, Location moveTo)
+        {
+            Location currentLocation = obj.Location;
+            Location endPoint;
+            int distance = 0;
+
+            // Check the type of the SpaceObject
+            if (obj is Pirate)
+            {
+                distance = GameSettings.Game.PirateMaxSpeed;
+            }
+            else if (obj is Asteroid)
+            {
+                distance = GameSettings.Game.AsteroidSpeed;
+            }
+            else
+                GameSettings.Game.Debug("The SpaceObject in PredictLocationAfterPush does not match any implemented SpaceObjects");
+
+            // Calculate the endPoint
+            endPoint = currentLocation.Towards(moveTo, distance);
+            endPoint = endPoint.Towards(pushTo, GameSettings.Game.PushDistance);
+
+            return endPoint;
+        }
     }
 }
