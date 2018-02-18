541f5c3be335d85d8b0a84420745b439b09e1b6f
idan elias <idanelias4@gmail.com>
2018-02-19 00:49:37 +0200

    merging somewhat thing

--- OverAbstractPirate/OverAbstractPirate/FieldInfo/Calculator.cs
+++ OverAbstractPirate/OverAbstractPirate/FieldInfo/Calculator.cs
@@ -74,100 +74,39 @@ namespace MyBot
         // Same as before, but now consider turns too
         public Location PredictLocationByMovement(SpaceObject obj, int turns)
         {
-            List<SpaceObject> spaceObjects = new List<SpaceObject>() ;
-            Location currentLocation = null;
-            if (obj.Location!=null)
-            {
-                currentLocation = obj.Location;
-                GameSettings.Game.Debug("obl location != null" + currentLocation);
-            }
-            else
-            {
-                GameSettings.Game.Debug("obl location == null");
-                return null;
-            }
+            List<SpaceObject> spaceObjects = null;
+            Location currentLocation = obj.Location;
             Location previousLocation = null;
-            // Location initialLocation = null;
 
             // First, check the type of obj
             if (obj is Pirate)
             {
-                Pirate pirate = obj as Pirate;
-                foreach (Pirate p in GameSettings.Game.GetEnemyLivingPirates())
-                {
-                    if (pirate.Equals(p))
-                    {
-                        
-                        previousLocation = GameSettings.LastGameEnemyPirates[pirate.Id];
-                        // initialLocation = p.InitialLocation;
-                        // GameSettings.Game.Debug("pervious location "+previousLocation);
-                    }
-                }
-                
-                foreach (Pirate p in GameSettings.Game.GetMyLivingPirates())
-                {
-                    if (pirate.Equals(p))
-                    {
-                        previousLocation = GameSettings.LastGameMyLivingPirates[p.Id];
-                        // initialLocation = p.InitialLocation;
-                        // GameSettings.Game.Debug("pervious location "+previousLocation);
-                    }
-                    
-                }
-                // spaceObjects.AddRange(GameSettings.LastGameMyLivingPirates[GameSettings..Count - 2].GetEnemyLivingPirates().ToList());
+                spaceObjects.AddRange(GameSettings.GameList[GameSettings.GameList.Count - 2].GetEnemyLivingPirates().ToList());
             }
             else if (obj is Asteroid)
             {
-                Asteroid asteroid = obj as Asteroid;
-
-                previousLocation = GameSettings.LastGameLivingAsteroids[asteroid.Id];
-                // initialLocation = asteroid.InitialLocation;
-                // GameSettings.Game.Debug("pervious location "+previousLocation);
-            }
-            else if (obj is Wormhole)
-            {
-                Wormhole wormhole = obj as Wormhole;
-                
-                previousLocation = GameSettings.LastGameWormholes[wormhole.Id];
-                // initialLocation = wormhole.InitialLocation;
-                // GameSettings.Game.Debug("pervious location "+previousLocation);
-                
-                // spaceObjects.AddRange(GameSettings.GameList[GameSettings.GameList.Count - 2].GetLivingAsteroids().ToList());
+                spaceObjects.AddRange(GameSettings.GameList[GameSettings.GameList.Count - 2].GetLivingAsteroids().ToList());
             }
             else return null;
 
-            /*foreach (SpaceObject movingObj in spaceObjects) // Check on every SpaceObject on the list defined before:
+            foreach (SpaceObject movingObj in spaceObjects) // Check on every SpaceObject on the list defined before:
             {
                 if (movingObj.Id == obj.Id) // Is it the original object?
                 {
                     previousLocation = movingObj.Location; // Then get it's past Location! put it in "previousLocation"
-                    // break;
+                    break;
                 }
-            }*/
+            }
 
             // Now, calculate it's predicted Location
-            GameSettings.Game.Debug("boo");
-            if (previousLocation != null)
+            Location substraction = currentLocation.Subtract(previousLocation);
+            for (int i = 0; i < turns; i++)
             {
-                GameSettings.Game.Debug("previousLocation != null");
-                Location substraction = currentLocation.Subtract(previousLocation);
-                Location nextLocation = currentLocation;
-                // GameSettings.Game.Debug("movment "+substraction);
-                for (int i = 0; i < turns; i++)
-                {
-                    nextLocation = nextLocation.Add(substraction);
-                }
-                // GameSettings.Game.Debug("next turns location "+nextLocation);
-                return nextLocation;
+                currentLocation.Add(substraction);
+
             }
-            else
-            {
-                GameSettings.Game.Debug("previousLocation == null");
-                return null;
-            }
-            
-            
-            
+
+            return currentLocation;
 
         }
 
@@ -176,7 +115,7 @@ namespace MyBot
         /// </summary>
         /// <param name="obj"></param>
         /// <returns></returns>
-        /*public Location PredictLocationByMovement(SpaceObject obj)
+        public Location PredictLocationByMovement(SpaceObject obj)
         {
             List<SpaceObject> spaceObjects = null;
             Location currentLocation = obj.Location;
@@ -205,7 +144,7 @@ namespace MyBot
             // Now, calculate it's predicted Location
             return currentLocation.Add(currentLocation.Subtract(previousLocation));
 
-        }*/
+        }
 
         public Location PredictLocationAfterPush(SpaceObject obj, Location pushTo, Location moveTo)
         {
@@ -223,12 +162,12 @@ namespace MyBot
                 distance = GameSettings.Game.AsteroidSpeed;
             }
             else
-                GameSettings.Game.Debug("The SpaceObject in PredictLocationAfterPush does not match any implemented SpaceObjects");
+               GameSettings.Game.Debug("The SpaceObject in PredictLocationAfterPush does not match any implemented SpaceObjects");
 
             // Calculate the endPoint
-            endPoint = currentLocation.Towards(moveTo, distance);
-            endPoint = endPoint.Towards(pushTo, GameSettings.Game.PushDistance);
-
+                endPoint = currentLocation.Towards(moveTo, distance);
+                endPoint = endPoint.Towards(pushTo, GameSettings.Game.PushDistance);
+            
             return endPoint;
         }
 
