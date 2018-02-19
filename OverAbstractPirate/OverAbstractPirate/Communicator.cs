8c269bae0391934156c280fa0d3d877fbd708ec3
idan elias <idanelias4@gmail.com>
2018-02-19 08:42:26 +0200

    changing encoding.. merged

--- OverAbstractPirate/OverAbstractPirate/Communicator.cs
+++ OverAbstractPirate/OverAbstractPirate/Communicator.cs
@@ -1,48 +1,53 @@
-﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Pirates;
+
 namespace MyBot
 {
-    /// <summary>
-    /// responsible for the communication between every strategy
-    /// + lets all strategys know all strategy's;
-    /// if a strategy needs to know something about other strategy, she can do it from here.
-    /// every action that needs to be done between strategy will be done here.
-    /// </summary>
-    public static class Communicator
+    public class Communicator
     {
+        private static Communicator instance;
+        private Communicator(){}
+        public static Communicator GetInstance()
+        {
+                if (instance == null)
+                {
+                    instance = new Communicator();
+                }
+                return instance;
+        }
         
         /// <summary>
         /// Every strategy in the game that needs to communicate with other strategys.
         /// </summary>
-        public static Formation formation;
-        public static FireWall fireWall;
+        public List<Strategy> StratList;
 
-        
+
         /// <summary>
         /// operational mathod, two pirates will swap.
         /// </summary>
         /// <param name="a">the pirate who swaps, he will be the one to be disabled</param>
         /// <param name="b">the pirate who is being swaped, he can continue with his life normally </param>
-        private static void MakeASwap(Pirate a, Pirate b)
+        private void MakeASwap(Pirate a, Pirate b)
         {
             a.SwapStates(b);
         }
         /// <summary>
         /// make sure that the defence gets all the heavys possible.
         /// </summary>
-        public static void GiveHeavysToDefence()
-        {
+        public bool GiveHeavysToDefence()
+        {//                 if(Participants.OfType<Carrier>().ToList() != null)
             List<BaseAttacker> heavyAttackers = new List<BaseAttacker>();
-            foreach(BaseAttacker attacker in formation.Participants.Cast<BaseAttacker>().ToList())
+            foreach(BaseAttacker attacker in StratList.OfType<Formation>().ToList()[0].Participants.Cast<BaseAttacker>().ToList())
             {
                 if(attacker.Pirate.StateName == "heavy")
                 {
                     heavyAttackers.Add(attacker);
                 }
+                GameSettings.Game.Debug("HEVY'S AREEEE " + attacker.Pirate.StateName);
             }
-                foreach(BaseDefender defender in fireWall.Participants.Cast<BaseDefender>().ToList())
+            
+                foreach(BaseDefender defender in StratList.OfType<FireWall>().ToList()[0].Participants.Cast<BaseDefender>().ToList())
                 {
                     if(defender.Pirate.StateName == "normal")
                     {
@@ -50,10 +55,11 @@ namespace MyBot
                         {
                             MakeASwap(defender.Pirate, heavyAttackers[0].Pirate);
                             heavyAttackers.RemoveAt(0);
+                            return true;
                         }
                     }
                 }
-            
+            return false;
         }
 
 
